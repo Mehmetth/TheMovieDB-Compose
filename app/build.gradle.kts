9d8ea1b490d6ec69b828a2487968141478e39b5f
MehmetP <mehmetpetek28@gmail.com>
2023-08-13 00:23:19 +0300

    - add implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -23,26 +25,70 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+        debug {
+            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
     }
+
+    productFlavors {
+        create("google") {
+            dimension = "platform"
+        }
+        create("live") {
+            dimension = "env"
+            applicationId = "com.mehmetpetek.themoviedb"
+            resValue("string", "app_name", "The Movie DB")
+            buildConfigField("String", "BASE_URL", "\"https://api.themoviedb.org/3/\"")
+            buildConfigField(
+                "String",
+                "API_TOKEN",
+                "YOUR_API_KEY_HERE"
+            )
+            buildConfigField("String", "IMAGE_BASE_URL", "\"https://image.tmdb.org/t/p/w500\"")
+        }
+        create("dev") {
+            dimension = "env"
+            applicationId = "com.mehmetpetek.themoviedb.beta"
+            resValue("string", "app_name", "The Movie DB - BETA")
+            buildConfigField("String", "BASE_URL", "\"https://api.themoviedb.org/3/\"")
+            buildConfigField(
+                "String",
+                "API_TOKEN",
+                "YOUR_API_KEY_HERE"
+            )
+            buildConfigField("String", "IMAGE_BASE_URL", "\"https://image.tmdb.org/t/p/w500\"")
+        }
+    }
+    flavorDimensions += listOf("platform", "env")
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
+        buildConfig = true
     }
+
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.6"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -51,7 +97,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.activity.compose)
@@ -60,6 +105,34 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+
+    implementation(libs.navigation)
+    implementation(libs.navigation.fragment)
+    implementation(libs.coil)
+
+    //Async Operations
+    implementation(libs.coroutines)
+
+    //DI
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+
+    //HTTP Request
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+
+    //HTTP Request Realtime Viewer
+    debugImplementation(libs.chucker)
+    releaseImplementation(libs.chucker.no.op)
+
+    //Exoplayer
+    implementation(libs.exoplayer)
+
+    //RoomDB - Offline Cache
+    implementation(libs.room.runtime)
+    kapt(libs.room.compiler)
+    implementation(libs.room.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
